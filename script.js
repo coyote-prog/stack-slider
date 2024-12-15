1d7851a41fb61c0a7056748003c9915a9de002ca
alex <electrod.alex314@gmail.com>
2024-12-15 21:20:32 +0300

    some refactor js

--- script.js
+++ script.js
@@ -1,82 +1,64 @@
 const stackBox = document.querySelector('.stack-box');
-
+const stackItems = document.querySelectorAll('.stack-item');
+const stackPrevBtn = document.querySelector('.stack-btn-prev');
+const stackNextBtn = document.querySelector('.stack-btn-next');
 
 const settings = {
   width: 200,
   height: 200,
   borderRadius: 10,
-  gap:30
+  gap: 30
 }
 
-function stackSliderStyling(settings) {
-  const stackItems = document.querySelectorAll('.stack-item');
-    
-  [...stackItems].forEach((item, index) =>{
-    /*let itemsIndex = Number(item.getAttribute('data-index'));*/
-    /*console.log(item.getAttribute('data-index'));*/
-    /*count ++;*/
+function stackSliderStyling() {
+
+  [...stackItems].forEach((item, index) => {
+
     item.style.width = settings.width + 'px';
     item.style.height = settings.height + 'px';
     item.style.borderRadius = settings.borderRadius + 'px';
     item.style.position = 'absolute';
     item.style.zIndex = index;
-    
+
     item.style.left = (settings.gap * index) + 'px';
     item.style.top = (settings.gap * index) + 'px';
   })
-
 }
 
-stackSliderStyling(settings);
+stackSliderStyling();
 
-/*function stackSliderSliding() {*/
-  const stackItems = document.querySelectorAll('.stack-item');
-  const stackPrevBtn = document.querySelector('.stack-btn-prev');
-const stackNextBtn = document.querySelector('.stack-btn-next');
 
-stackNextBtn.addEventListener('click', ()=>{
-  /*count++;*/
-  [...stackItems].forEach((item) =>{
-    /*let itemsIndex = Number(item.getAttribute('data-index'));*/
-    
-  /*itemsIndex++;*/
-  item.style.zIndex = Number(item.style.zIndex) + 1;
-  /*console.log(item.getAttribute('data-index'));*/
-  
-  if((Number(item.style.zIndex)) >= stackItems.length) {
-    item.style.zIndex = 0;
-  }
-  
+stackNextBtn.addEventListener('click', () => {
+
+  [...stackItems].forEach((item) => {
+
+    item.style.zIndex = Number(item.style.zIndex) + 1;
+
+    if ((Number(item.style.zIndex)) >= stackItems.length) {
+      item.style.zIndex = 0;
+    }
+
     item.style.left = (settings.gap * (Number(item.style.zIndex))) + 'px';
-      item.style.top = (settings.gap * (Number(item.style.zIndex))) + 'px';
-  
-    });
-/*console.log(stackItems);*/
+    item.style.top = (settings.gap * (Number(item.style.zIndex))) + 'px';
 
   });
- 
-/*}*/
 
-stackPrevBtn.addEventListener('click', ()=>{
-  /*count++;*/
-  [...stackItems].forEach((item) =>{
-    /*let itemsIndex = Number(item.getAttribute('data-index'));*/
-    
-  /*itemsIndex++;*/
-  item.style.zIndex = Number(item.style.zIndex) - 1;
-  /*console.log(item.getAttribute('data-index'));*/
-  
-  /*if((Number(item.style.zIndex)) >= stackItems.length) {
-    item.style.zIndex = 0;
-  }*/
-  if((Number(item.style.zIndex)) < 0) {
-    item.style.zIndex = stackItems.length - 1;
-  }
-  
+});
+
+
+stackPrevBtn.addEventListener('click', () => {
+
+  [...stackItems].forEach((item) => {
+
+    item.style.zIndex = Number(item.style.zIndex) - 1;
+
+    if ((Number(item.style.zIndex)) < 0) {
+      item.style.zIndex = stackItems.length - 1;
+    }
+
     item.style.left = (settings.gap * (Number(item.style.zIndex))) + 'px';
-      item.style.top = (settings.gap * (Number(item.style.zIndex))) + 'px';
-  
-    });
-/*console.log(stackItems);*/
+    item.style.top = (settings.gap * (Number(item.style.zIndex))) + 'px';
 
   });
+
+});
