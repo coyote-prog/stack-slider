bcc3a43c6bbf3d85f0a67bd12cfcebfe11dae1f7
alex <electrod.alex314@gmail.com>
2024-12-15 20:55:26 +0300

    remove data-attribute in HTML, add functionality in JS

--- script.js
+++ script.js
@@ -10,15 +10,17 @@ const settings = {
 
 function stackSliderStyling(settings) {
   const stackItems = document.querySelectorAll('.stack-item');
-  let count = 0;
-
+    
   [...stackItems].forEach((item, index) =>{
-    count ++;
+    /*let itemsIndex = Number(item.getAttribute('data-index'));*/
+    /*console.log(item.getAttribute('data-index'));*/
+    /*count ++;*/
     item.style.width = settings.width + 'px';
     item.style.height = settings.height + 'px';
     item.style.borderRadius = settings.borderRadius + 'px';
     item.style.position = 'absolute';
     item.style.zIndex = index;
+    
     item.style.left = (settings.gap * index) + 'px';
     item.style.top = (settings.gap * index) + 'px';
   })
@@ -27,3 +29,54 @@ function stackSliderStyling(settings) {
 
 stackSliderStyling(settings);
 
+/*function stackSliderSliding() {*/
+  const stackItems = document.querySelectorAll('.stack-item');
+  const stackPrevBtn = document.querySelector('.stack-btn-prev');
+const stackNextBtn = document.querySelector('.stack-btn-next');
+
+stackNextBtn.addEventListener('click', ()=>{
+  /*count++;*/
+  [...stackItems].forEach((item) =>{
+    /*let itemsIndex = Number(item.getAttribute('data-index'));*/
+    
+  /*itemsIndex++;*/
+  item.style.zIndex = Number(item.style.zIndex) + 1;
+  /*console.log(item.getAttribute('data-index'));*/
+  
+  if((Number(item.style.zIndex)) >= stackItems.length) {
+    item.style.zIndex = 0;
+  }
+  
+    item.style.left = (settings.gap * (Number(item.style.zIndex))) + 'px';
+      item.style.top = (settings.gap * (Number(item.style.zIndex))) + 'px';
+  
+    });
+/*console.log(stackItems);*/
+
+  });
+ 
+/*}*/
+
+stackPrevBtn.addEventListener('click', ()=>{
+  /*count++;*/
+  [...stackItems].forEach((item) =>{
+    /*let itemsIndex = Number(item.getAttribute('data-index'));*/
+    
+  /*itemsIndex++;*/
+  item.style.zIndex = Number(item.style.zIndex) - 1;
+  /*console.log(item.getAttribute('data-index'));*/
+  
+  /*if((Number(item.style.zIndex)) >= stackItems.length) {
+    item.style.zIndex = 0;
+  }*/
+  if((Number(item.style.zIndex)) < 0) {
+    item.style.zIndex = stackItems.length - 1;
+  }
+  
+    item.style.left = (settings.gap * (Number(item.style.zIndex))) + 'px';
+      item.style.top = (settings.gap * (Number(item.style.zIndex))) + 'px';
+  
+    });
+/*console.log(stackItems);*/
+
+  });
